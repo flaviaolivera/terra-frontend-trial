1d7396bc97bf46f9f860cb2bed78fc4c9ed18581
Flavia <fernandezoliveraflavia@gmail.com>
2025-08-08 14:48:55 +0200

    feat: initial project setup

---
+++ src/js/Main.js
@@ -0,0 +1,71 @@
+import Collapsify from "@terrahq/collapsify";
+import RevealItem from "./modules/RevealItem";
+class Main {
+    constructor(options = {}) {
+        this.DOM = {
+            revealItems: document.querySelectorAll(".js--reveal-item"),
+            playBtn: document.querySelector(".js--play"),
+            pauseBtn: document.querySelector(".js--pause"),
+            lottieElements: document.querySelectorAll(".js--lottie-element"),
+        }
+        this.instances = []
+        this.lottieInstances = options.lottieInstances || []
+        this.init();
+        this.events()
+    }
+
+    init() {
+
+        this.instances['collapsify'] = [];
+        this.instances['collapsify'][0] = new Collapsify({
+            onToggle: (isOpen, element) => {
+                this.handleAccordionToggle(isOpen, element);
+            }
+        });
+
+
+        this.instances['RevealInstances'] = [];
+        this.DOM.revealItems.forEach((item, index) => {
+            this.instances['RevealInstances'][index] = new RevealItem({ element: item });
+        });
+
+    }
+
+    events() {
+        if (this.DOM.playBtn) {
+            this.DOM.playBtn.addEventListener("click", () => {
+                this.lottieInstances.forEach(instance => {
+                    if (instance && typeof instance.play === 'function') {
+                        instance.play();
+                    }
+                });
+            });
+        }
+
+        if (this.DOM.pauseBtn) {
+            this.DOM.pauseBtn.addEventListener("click", () => {
+                this.lottieInstances.forEach(instance => {
+                    if (instance && typeof instance.pause === 'function') {
+                        instance.pause();
+                    }
+                });
+            });
+        }
+    }
+
+    handleAccordionToggle(isOpen, element) {
+        const button = element.querySelector('.c--accordion-a__item__hd');
+        const arrow = button.querySelector('.c--accordion-a__item__hd__artwork');
+
+        if (arrow) {
+            if (isOpen) {
+                arrow.style.transform = 'rotate(180deg)';
+                button.setAttribute('aria-expanded', 'true');
+            } else {
+                arrow.style.transform = 'rotate(0deg)';
+                button.setAttribute('aria-expanded', 'false');
+            }
+        }
+    }
+}
+export default Main;
